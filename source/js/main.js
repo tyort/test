72e9c547e38fb42a4ab25b60367b202af0579b10
tyort <neotyo1986@gmail.com>
2020-07-29 09:51:13 +0300

    header

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,21 @@
+const pageHeader = document.querySelector(`.page-header`);
+const pageHeaderMenuIcon = pageHeader.querySelector(`.page-header__menu-icon`);
 
+pageHeaderMenuIcon.addEventListener(`click`, function () {
+  document.addEventListener(`keydown`, onEscKeyDown);
 
+  if (pageHeader.querySelector(`.menu-icon__lines`).classList.contains(`lines__active`)) {
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  }
 
+  pageHeader.querySelector(`.mobile-main-navigation`).classList.toggle(`navigation-active`);
+  pageHeader.querySelector(`.menu-icon__lines`).classList.toggle(`lines__active`);
+});
+
+function onEscKeyDown(evt) {
+  if (evt.key === `Escape` || evt.key === `Esc`) {
+    pageHeader.querySelector(`.mobile-main-navigation`).classList.toggle(`navigation-active`, false);
+    pageHeader.querySelector(`.menu-icon__lines`).classList.toggle(`lines__active`, false);
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  }
+}
