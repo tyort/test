9ab77dd99b1f9157255d38194702d92ee25934f3
tyort <neotyo1986@gmail.com>
2020-08-03 16:49:31 +0300

    шапка рефакторинг

--- build/js/main.js
+++ build/js/main.js
@@ -3,25 +3,39 @@
 
   const pageHeader = document.querySelector(`.page-header`);
   const pageHeaderMenuIcon = pageHeader.querySelector(`.page-header__menu-icon`);
+  const headerLogo = document.querySelector(`.page-header__logo`).querySelector(`img`);
 
   pageHeaderMenuIcon.addEventListener(`click`, function () {
     document.addEventListener(`keydown`, onEscKeyDown);
+    setTimeout(() => (headerLogo.src = `img/header-logo-black.svg`), 200);
 
     if (pageHeader.querySelector(`.menu-icon__lines`).classList.contains(`lines__active`)) {
       document.removeEventListener(`keydown`, onEscKeyDown);
+
+      setTimeout(() => (headerLogo.src = `img/logo.svg`), 200);
     }
 
-    pageHeader.querySelector(`.mobile-main-navigation`).classList.toggle(`navigation-active`);
+    pageHeader.querySelector(`.main-navigation`).classList.toggle(`navigation-active`);
     pageHeader.querySelector(`.menu-icon__lines`).classList.toggle(`lines__active`);
   });
 
   function onEscKeyDown(evt) {
     if (evt.key === `Escape` || evt.key === `Esc`) {
-      pageHeader.querySelector(`.mobile-main-navigation`).classList.toggle(`navigation-active`, false);
+      pageHeader.querySelector(`.main-navigation`).classList.toggle(`navigation-active`, false);
       pageHeader.querySelector(`.menu-icon__lines`).classList.toggle(`lines__active`, false);
       document.removeEventListener(`keydown`, onEscKeyDown);
+      setTimeout(() => (headerLogo.src = `img/logo.svg`), 200);
     }
   }
+
+  window.addEventListener(`resize`, () => {
+    if (window.innerWidth >= 728) {
+      pageHeader.querySelector(`.main-navigation`).classList.toggle(`navigation-active`, false);
+      pageHeader.querySelector(`.menu-icon__lines`).classList.toggle(`lines__active`, false);
+      document.removeEventListener(`keydown`, onEscKeyDown);
+      headerLogo.src = `img/logo.svg`;
+    }
+  });
 
   window.addEventListener(`mapWasLoaded`, () => {
     window.ymaps.ready(init);
