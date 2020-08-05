f446a1b241223a3a50ebb268cba1a9cc28ce580b
tyort <neotyo1986@gmail.com>
2020-08-06 01:43:12 +0300

    валидация формы

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,38 @@
   const menu = document.querySelector(`#check-menu`);
   const agreement = document.querySelector(`.field-agreement`).querySelector(`input`);
   const btn = document.querySelector(`.page-booking__form`).querySelector(`button`);
+  const form = document.querySelector(`.page-booking__form`);
+
+
+  form.addEventListener(`input`, (evt) => {
+    const phoneSample = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+    const mailSample = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    const nameSample = /^[a-zA-Zа-яёА-ЯЁ]+$/u;
+
+    if (evt.target.className === `field-phone__input`) {
+      if (!phoneSample.test(evt.target.value)) {
+        evt.target.setCustomValidity(`Напиши номер правильно`);
+
+      } else {
+        evt.target.setCustomValidity(``);
+      }
+
+    } else if (evt.target.className === `field-email__input`) {
+      if (!mailSample.test(evt.target.value)) {
+        evt.target.setCustomValidity(`Напиши email правильно`);
+
+      } else {
+        evt.target.setCustomValidity(``);
+      }
+    } else if (evt.target.className === `field-name__input`) {
+      if (!nameSample.test(evt.target.value)) {
+        evt.target.setCustomValidity(`Напиши ФИО правильно`);
+
+      } else {
+        evt.target.setCustomValidity(``);
+      }
+    }
+  });
 
   agreement.addEventListener(`change`, (evt) => {
     if (evt.target.checked) {
