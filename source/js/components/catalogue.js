488564ffca221d972d733639c600978e8683533f
tyort <neotyo1986@gmail.com>
2020-08-31 19:57:10 +0300

    svg

--- source/js/components/catalogue.js
+++ source/js/components/catalogue.js
@@ -1,53 +1,8 @@
-(function () {
-  'use strict';
+import AbstractComponent from './abstract-component.js';
 
-  const renderComponent = (container, element, place) => {
-    switch (place) {
-      case `afterBegin`:
-        container.prepend(element.getElement());
-        break;
-      case `afterEnd`:
-        container.after(element.getElement());
-        break;
-      default:
-        container.append(element.getElement());
-    }
-  };
-
-  const createElement = (template) => {
-    const newElement = document.createElement(`div`);
-    newElement.innerHTML = template;
-    return newElement.firstChild;
-  };
-
-  class AbstractComponent {
-    constructor() {
-      if (new.target === AbstractComponent) {
-        throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
-      }
-
-      this._element = null;
-    }
-
-    getTemplate() {
-      throw new Error(`Abstract method not implemented: getTemplate`);
-    }
-
-    getElement() {
-      if (!this._element) {
-        this._element = createElement(this.getTemplate());
-      }
-      return this._element;
-    }
-
-    removeElement() {
-      this._element = null;
-    }
-  }
-
-  const createCatalogueTemplate = () =>
-    (
-      `<div class="page-catalogue">
+const createCatalogueTemplate = () =>
+  (
+    `<div class="page-catalogue">
       <div class="page-catalogue__inner">
         <h2>Программы</h2>
         <div class="catalogue-details">
@@ -88,18 +43,10 @@
         </div>
       </div>
     </div>`
-    );
+  );
 
-  class Catalogue extends AbstractComponent {
-    getTemplate() {
-      return createCatalogueTemplate();
-    }
+export default class Catalogue extends AbstractComponent {
+  getTemplate() {
+    return createCatalogueTemplate();
   }
-
-  const main = document.querySelector(`main`);
-  const catalogue = new Catalogue();
-  renderComponent(main, catalogue);
-
-}());
-
-//# sourceMappingURL=main.js.map
+}
