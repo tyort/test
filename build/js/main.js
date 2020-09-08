4cf05caa6d26488cfd011e1faddedd16c695f6f9
tyort <neotyo1986@gmail.com>
2020-09-08 20:48:33 +0300

    вариативность слайдера

--- build/js/main.js
+++ build/js/main.js
@@ -684,6 +684,53 @@
     }
   }
 
+  const list = document.querySelector(`.live-pictures`);
+  const listMobile = document.querySelector(`.live-pictures--mobile`);
+
+  window.$(document).ready(() => {
+    window.$(`.live-pictures--mobile`).slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: false,
+      swipe: true,
+      focusOnSelect: true,
+      centerPadding: `20%`,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+          }
+        }
+      ]
+    });
+  });
+
+  document.addEventListener(`DOMContentLoaded`, function () {
+    if (window.innerWidth >= 768) {
+      list.classList.toggle(`visually-hidden`, false);
+      listMobile.classList.toggle(`visually-hidden`, true);
+
+    } else {
+      list.classList.toggle(`visually-hidden`, true);
+      listMobile.classList.toggle(`visually-hidden`, false);
+    }
+  });
+
+  window.addEventListener(`resize`, () => {
+    if (window.innerWidth >= 768) {
+      list.classList.toggle(`visually-hidden`, false);
+      listMobile.classList.toggle(`visually-hidden`, true);
+
+    } else {
+      list.classList.toggle(`visually-hidden`, true);
+      listMobile.classList.toggle(`visually-hidden`, false);
+    }
+  });
+
   const body = document.querySelector(`body`);
   const main = document.querySelector(`main`);
   const about = body.querySelector(`.page-about`);
--- source/js/components/live.js
+++ source/js/components/live.js
@@ -1,13 +1,46 @@
-// const list = document.querySelector(`.live-pictures`);
+const list = document.querySelector(`.live-pictures`);
+const listMobile = document.querySelector(`.live-pictures--mobile`);
 
-// window.$(document).ready(() => {
-//   window.$(`.live-pictures`).slick({
-//     dots: false,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     centerMode: true,
-//     swipe: true,
-//     focusOnSelect: true
-//   });
-// });
+window.$(document).ready(() => {
+  window.$(`.live-pictures--mobile`).slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: false,
+    swipe: true,
+    focusOnSelect: true,
+    centerPadding: `20%`,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        }
+      }
+    ]
+  });
+});
+
+document.addEventListener(`DOMContentLoaded`, function () {
+  if (window.innerWidth >= 768) {
+    list.classList.toggle(`visually-hidden`, false);
+    listMobile.classList.toggle(`visually-hidden`, true);
+
+  } else {
+    list.classList.toggle(`visually-hidden`, true);
+    listMobile.classList.toggle(`visually-hidden`, false);
+  }
+});
+
+window.addEventListener(`resize`, () => {
+  if (window.innerWidth >= 768) {
+    list.classList.toggle(`visually-hidden`, false);
+    listMobile.classList.toggle(`visually-hidden`, true);
+
+  } else {
+    list.classList.toggle(`visually-hidden`, true);
+    listMobile.classList.toggle(`visually-hidden`, false);
+  }
+});
