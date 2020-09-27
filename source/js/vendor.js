149b9bd4feb9f7d6384a3c92f2365f33a50f9ded
tyort <neotyo1986@gmail.com>
2020-09-28 00:14:39 +0300

    карта

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -2,25 +2,15 @@ import $ from "jquery";
 window.$ = $;
 import 'slick-carousel';
 
-let YaMapsShown = false;
+const yandexMapsScript = document.createElement(`script`);
+const key = `6b492866-f739-4d69-9dbc-9ff50d70ea08`;
 
-window.$(document).ready(function () {
-  window.$(window).scroll(function () {
-    if (!YaMapsShown) {
-      if (window.$(window).scrollTop() + window.$(window).height() > window.$(document).height() - 700) {
-        showYaMaps();
-        YaMapsShown = true;
-      }
-    }
-  });
+yandexMapsScript.src = `https://api-maps.yandex.ru/2.1/?apikey=${key}&lang=ru_RU`;
+
+document.body.appendChild(yandexMapsScript);
+
+yandexMapsScript.addEventListener(`load`, function () {
+  window.dispatchEvent(new Event(`mapWasLoaded`));
 });
-
-function showYaMaps() {
-  const yandexMapsScript = document.createElement(`script`);
-  const key = `6b492866-f739-4d69-9dbc-9ff50d70ea08`;
-  yandexMapsScript.type = `text/javascript`;
-  yandexMapsScript.src = `https://api-maps.yandex.ru/2.1/?apikey=${key}&lang=ru_RU`;
-  document.body.appendChild(yandexMapsScript);
-}
 
 
