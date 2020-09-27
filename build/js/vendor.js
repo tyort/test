1dc75ff083962cd65ee7ca4ace618aefc65bc838
tyort <neotyo1986@gmail.com>
2020-09-28 00:00:18 +0300

    подгрузка шрифтов

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -13840,16 +13840,26 @@
 
 	window.$ = jquery;
 
-	const yandexMapsScript = document.createElement(`script`);
-	const key = `6b492866-f739-4d69-9dbc-9ff50d70ea08`;
+	let YaMapsShown = false;
 
-	yandexMapsScript.src = `https://api-maps.yandex.ru/2.1/?apikey=${key}&lang=ru_RU`;
-
-	document.body.appendChild(yandexMapsScript);
-
-	yandexMapsScript.addEventListener(`load`, function () {
-	  window.dispatchEvent(new Event(`mapWasLoaded`));
+	window.$(document).ready(function () {
+	  window.$(window).scroll(function () {
+	    if (!YaMapsShown) {
+	      if (window.$(window).scrollTop() + window.$(window).height() > window.$(document).height() - 700) {
+	        showYaMaps();
+	        YaMapsShown = true;
+	      }
+	    }
+	  });
 	});
+
+	function showYaMaps() {
+	  const yandexMapsScript = document.createElement(`script`);
+	  const key = `6b492866-f739-4d69-9dbc-9ff50d70ea08`;
+	  yandexMapsScript.type = `text/javascript`;
+	  yandexMapsScript.src = `https://api-maps.yandex.ru/2.1/?apikey=${key}&lang=ru_RU`;
+	  document.body.appendChild(yandexMapsScript);
+	}
 
 }());
 
