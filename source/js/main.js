f966d1a84d4088a7a8f459aa7ac950a3bb5bd719
tyort <neotyo1986@gmail.com>
2020-10-09 16:06:47 +0300

    возможность добавлять картинку

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ const pageLink = document.querySelector(`.page-link`);
 const photoGallery = document.querySelector(`.photo-gallery`);
 
 import {sendRequest, renderComponent, createElement} from './formulas';
+import './drag-n-drop';
 
 pageLink.querySelector(`form`).addEventListener(`submit`, (evt) => {
   evt.preventDefault();
@@ -74,92 +75,6 @@ const onPhotoClick = (evt) => {
   photoContainer.remove();
 }
 
-
-const dropArea = document.querySelector(`#drop-area`);
-const movingEvents = [`dragenter`, `dragover`, `dragleave`, `drop`];
-
-movingEvents.forEach((eventName) => {
-  dropArea.addEventListener(eventName, preventDefaults)
-})
-
-function preventDefaults (evt) {
-  evt.preventDefault()
-  evt.stopPropagation()
-}
-
-movingEvents.slice(0, 2).forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight)
-})
-movingEvents.slice(2, 5).forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight)
-})
-function highlight() {
-  dropArea.classList.add('highlight')
-}
-function unhighlight() {
-  dropArea.classList.remove('highlight')
-}
-
-dropArea.addEventListener(`drop`, handleDrop)
-function handleDrop(e) {
-  let dt = e.dataTransfer
-  let files = dt.files
-  handleFiles(files)
-}
-
-function handleFiles(files) {
-  files = [...files]
-  initializeProgress(files.length) // <- Добавили эту строку
-  files.forEach(uploadFile)
-  files.forEach(previewFile)
-}
-
-function uploadFile(file, i) { // <- Добавили параметр `i`
-  var url = 'http://192.168.43.222:8080'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', url, true)
-  // Добавили следующие слушатели
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-  })
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // Готово. Сообщаем пользователю
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Ошибка. Сообщаем пользователю
-    }
-  })
-  formData.append('file', file)
-  xhr.send(formData)
-}
-
-function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
-}
-
-let uploadProgress = [];
-let progressBar = document.getElementById('progress-bar')
-
-function initializeProgress(numFiles) {
-  progressBar.value = 0
-  uploadProgress = []
-  for(let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0)
-  }
-}
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  progressBar.value = total
-}
 
 
 
